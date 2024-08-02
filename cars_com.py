8fada2039d840dc9835134417b80b513e5de59f6
Hunter Peterson <69177906+hunterycz@users.noreply.github.com>
2024-08-02 14:04:13 -0700

    finished the url generator for the autotrader.py module

--- cars_com.py
+++ cars_com.py
@@ -4,10 +4,21 @@ class CarCom:
         self.max_price = max_price
         self.min_price = min_price
         self.distance = distance
+        self.carcom_car_ids = {
+            "toyota-highlander": "toyota",
+            "toyota-rav4": "toyota",
+            "honda-cr-v": "honda",
+            "subaru-impreza": "subaru",
+            "subaru_forester": "subaru",
+            "subaru_outback": "subaru"
+        }
 
 
     def carwebsite_url_maker(self, car_id):
-        return www.yougay.com
+        return f""
     
     def generate_urls(self):
         return urls and you're gay'
+
+
+return f"https://www.cars.com/shopping/results/?clean_title=true&dealer_id=&keyword=&list_price_max={self.max_price}&list_price_min={self.min_price}&makes[]={subaru}&maximum_distance={self.distance}&mileage_max=&models[]={subaru-impreza}&no_accidents=true&sort=best_match_desc&stock_type=all&year_max=&year_min=&zip={self.zip_code}"
