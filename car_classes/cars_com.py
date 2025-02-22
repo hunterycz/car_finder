060bd0ac435f98f28f686162fc1407bd9d5164fe
Hunter Peterson <69177906+hunterycz@users.noreply.github.com>
2025-02-22 09:29:28 -0700

    edited the cars com module

--- car_classes/cars_com.py
+++ car_classes/cars_com.py
@@ -1,6 +1,6 @@
 class CarsCom:
     """
-    CarsCom class is used to generate URLs for car listings based on specific criteria.
+    CarsCom class is used to generate URLs for car listings based on specific criteria
 
     This class takes in a zip code, maximum and minimum price, and a distance in miles from your current location,
     and then creates URLs for all the cars listed/saved inside of the class.
